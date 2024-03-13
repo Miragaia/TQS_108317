bc38f3317a1e5dbdcd4340493d5a0e08eadde673
Miragaia <miguelmiragaia2003@gmail.com>
2024-03-13 10:44:53 +0000

    lab 4_1

---
+++ lab4/lab3_1hello_world/src/test/java/com/example/AppTestRefactor.java
@@ -0,0 +1,47 @@
+package com.example;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+
+import io.github.bonigarcia.seljup.DockerBrowser;
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import static io.github.bonigarcia.seljup.BrowserType.CHROME;
+
+@ExtendWith(SeleniumJupiter.class)
+class AppTestRefactor {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    @Test
+    void test_refactor(ChromeDriver driver) {
+        // Exercise
+        String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+        log.debug("The title of {} is {}", sutUrl, title);
+
+        // Verify
+        assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
+    }
+
+    @Test
+    void test_refactor_chrome(@DockerBrowser(type = CHROME) WebDriver driver) {
+        // Exercise
+        String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+        log.debug("The title of {} is {}", sutUrl, title);
+
+        // Verify
+        assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
+    }
+
+}
